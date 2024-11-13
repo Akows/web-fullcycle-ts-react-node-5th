56ce7ce51d4727add8b7295488d6fdc403784941
AKOWSPC <akows141@gmail.com>
2024-11-13 11:07:27 +0900

    init: .gitignore 설정에 맞춰 파일 재업로드.

--- prj/server/src/index.js
+++
@@ -1,5 +0,0 @@
-const { startServer } = require('./server');
-const router = require('./routes/routes');
-const { handle } = require('./requestHandler/requestHandler'); 
-
-startServer(3000, router.route, handle);
