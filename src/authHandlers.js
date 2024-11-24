099c7f933caf504e5399cc64944f11d865c21fd2
AKOWSPC <akows141@gmail.com>
2024-11-24 17:49:56 +0900

    learn: 미니 프로젝트 01 시작 - 프로젝트 구조 구현 - 회원/채널 기능쪽의 라우터와 핸들러의 기본 구조를 구현.

---
+++ src/authHandlers.js
@@ -0,0 +1,22 @@
+const loginMember = (req, res) => {
+    
+};
+
+const createMember = (req, res) => {
+    
+};
+
+const getMemberData = (req, res) => {
+    const id = req.param.id;
+};
+
+const deleteMember = (req, res) => {
+    const id = req.param.id;
+};
+
+module.exports = { 
+    loginMember,
+    createMember,
+    getMemberData,
+    deleteMember
+};
---
+++ src/channelHandlers.js
@@ -0,0 +1,17 @@
+const createChannel  = (req, res) => {
+    
+};
+
+const updateChannel = (req, res) => {
+    const id = req.param.id;
+};
+
+const deleteChannel = (req, res) => {
+    const id = req.param.id;
+};
+
+module.exports = { 
+    createChannel,
+    updateChannel,
+    deleteChannel
+};
