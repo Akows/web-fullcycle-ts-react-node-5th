3dc6304818511a16315d7573367798e0e91b2bda
AKOWSPC <akows141@gmail.com>
2024-11-24 14:08:58 +0900

    learn: 241124 수업내용 업데이트 - 과일 데이터 조회 기능 - 편의를 위해 라우터, 핸들러, 데이터 파일을 분리

---
+++ src/data.js
@@ -0,0 +1,11 @@
+const fruits = [
+    {id: 1, name: 'apple'},
+    {id: 2, name: 'banana'},
+    {id: 3, name: 'orange'},
+    {id: 4, name: 'blueberry'},
+    {id: 5, name: 'dragon fruit'},
+]
+
+module.exports = {
+    fruits
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
-const serverStart =  (req, res) => {
-    console.log('서버가 정상적으로 가동되었습니다.');
-    console.log('환영합니다.');
-}
+const express = require('express');
+const router = express.Router();
 
-module.exports = {
-    serverStart,
-};
+const { getFruits, getFruit } = require('./fruitHandlers');
+
+router.get('/', (req, res) => {
+    console.log('서버가 정상적으로 가동되었습니다.');
+    res.send('환영합니다.');
+});
+
+router.get("/getfruits", getFruits);
+router.get('/getfruit/:id', getFruit);
+
+module.exports = router;
