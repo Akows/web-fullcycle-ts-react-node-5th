0c1353d4fe5e6d6ab41072189ee99b9a6c653722
AKOWSPC <akows141@gmail.com>
2024-11-25 13:56:10 +0900

    learn - Feat: 미니 프로젝트 진행 - 로그인 기능 구현 - 유효성 검사 및 예외처리 포함, 정상 동작을 확인함.

--- src/handlers/memberHandlers.js
+++ src/handlers/memberHandlers.js
@@ -1,9 +1,34 @@
-const { memberDatabase } = require("../data/data");
+const { memberDatabase } = require('../data/data');
 
 let id = 1;
 
 const loginMember = (req, res) => {
-    
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+        res.status(400).json({ message: '로그인 요청 중 에러가 발생하였습니다.' });
+    };
+
+    const { name, password } = req.body;
+
+    if (!name || !password) {
+        res.status(400).json({ message: '이름과 비밀번호를 모두 입력해주세요.' });
+    };
+
+    let foundMember = false;
+    let userName = '';
+    memberDatabase.forEach((member, __) => {
+        if (member.name === name && member.password === password) {
+            foundMember = true;
+            userName = member.name;
+        }
+    });
+
+    if (foundMember) {
+        res.status(200).json({ message: '로그인 성공!', user: userName });
+    } 
+    else {
+        res.status(401).json({ message: '이름 또는 비밀번호가 올바르지 않습니다.' });
+    }
 };
 
 const createMember = (req, res) => {
@@ -24,7 +49,7 @@ const getMemberData = (req, res) => {
         res.status(200).json({ id, ...member });
     } 
     else {
-        res.status(404).json({ message: "회원 정보를 찾을 수 없습니다." });
+        res.status(404).json({ message: '회원 정보를 찾을 수 없습니다.' });
     }
 };
 
