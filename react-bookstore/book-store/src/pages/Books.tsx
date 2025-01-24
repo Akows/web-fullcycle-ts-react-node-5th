ac07d9c95b5c80cd4be61d3df063551550a27555
AKOWSPC <akows141@gmail.com>
2025-01-24 13:49:37 +0900

    Feat: 프로젝트: React(TypeScript) 기반의 동적 UI 개발 - Store App 코드 파일 업로드.

---
+++ react-bookstore/book-store/src/pages/Books.tsx
@@ -0,0 +1,84 @@
+import { styled } from "styled-components";
+import Title from "../components/common/Title";
+import BooksFilter from "../components/books/BooksFilter";
+import BooksList from "../components/books/BooksList";
+import BooksEmpty from "../components/books/BooksEmpty";
+import BooksViewSwitcher from "../components/books/BooksViewSwitcher";
+import { useBooksInfinite } from "@/hooks/useBooksInfinite";
+import Loading from "@/components/common/Loading";
+import Button from "@/components/common/Button";
+import { Book } from "@/models/book.model";
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+
+function Books() {
+    const {
+        books,
+        pagination,
+        isEmpty,
+        isBooksLoading,
+        fetchNextPage,
+        hasNextPage,
+    } = useBooksInfinite();
+
+    const moreRef = useIntersectionObserver(([entry]) => {
+        if (entry.isIntersecting) {
+            loadMore();
+        }
+    });
+
+    const loadMore = () => {
+        if (!hasNextPage) return;
+        fetchNextPage();
+    }
+
+    if (isEmpty) {
+        return <BooksEmpty />;
+    }
+
+    if (!books || !pagination || isBooksLoading) {
+        return <Loading />;
+    }
+
+    // undefined 값을 필터링하여 Book[] 배열을 생성
+    const filteredBooks = books.filter((book): book is Book => book !== undefined);
+
+    return (
+        <>
+            <Title size="large">도서 검색 결과</Title>
+            <BooksStyle>
+                <div className="filter">
+                    <BooksFilter />
+                    <BooksViewSwitcher />
+                </div>
+                <BooksList books={filteredBooks} />
+                {/* <Pagination pagination={pagination}/> */}
+                <div className="more" ref={moreRef}>
+                    <Button
+                        size="medium"
+                        scheme="normal"
+                        onClick={() => fetchNextPage()}
+                        disabled={!hasNextPage}
+                    >
+                        {hasNextPage ? '더보기' : '마지막 페이지'}
+                    </Button>
+                </div>
+            </BooksStyle>
+        </>
+    );
+}
+
+const BooksStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 24px;
+
+    .filter {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 20px 0;
+    }
+`;
+
+export default Books;
---
+++ react-bookstore/book-store/src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import Title from "@/components/common/Title";
+import Banner from "@/components/common/banner/Banner";
+import MainBest from "@/components/main/MainBest";
+import MainNewBooks from "@/components/main/MainNewBooks";
+import MainReview from "@/components/main/MainReview";
+import { useMain } from "@/hooks/useMain";
+import { styled } from "styled-components";
+
+function Home() {
+    const { reviews, newBooks, bestBooks, banners } = useMain();
+    return (
+        <HomeStyle>
+            {/* 베너 */}
+            <Banner banners={banners}/>
+
+            {/* 베스트셀러 */}
+            <section className="section">
+                <Title size="large">베스트 셀러</Title>
+                <MainBest books={bestBooks} />
+            </section>
+
+            {/* 신간 */}
+            <section className="section">
+                <Title size="large">신간 안내</Title>
+                <MainNewBooks books={newBooks} />
+            </section>
+
+            {/* 리뷰 */}
+            <section className="section">
+                <Title size="large">리뷰</Title>
+                <MainReview reviews={reviews} />
+            </section>
+        </HomeStyle>
+    );
+}
+
+const HomeStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+`;
+
+export default Home;
